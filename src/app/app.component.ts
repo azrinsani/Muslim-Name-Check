9ed7635985f2d71fb7fe618ce23823a73d856fa9
Azrin Sani <azrinsani@gmail.com>
2022-01-29 22:02:14 +0800

    Added Search View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MuslimNameCheck';
+  namesToCheck = '';
+  searchDisable:boolean = false;
+  showLoading = false;
+
+  emitNamesToCheck(el: any) {
+    console.log(el.value);
+    this.title=el.value || '';
+  }
+
+  checkClick() {
+    console.log(this.namesToCheck);
+    this.searchDisable = true;
+    this.showLoading = true;
+
+  }
+
+  ngOnInit() {
+    console.log('App Started');
+  }
+
 }
