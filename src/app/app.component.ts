34f1b0c7cd7d452b2dc5711de68a814bf015221b
Azrin Sani <azrinsani@gmail.com>
2022-01-31 14:15:37 +0800

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,9 @@ export class AppComponent implements OnInit {
   searchDisable:boolean = false;
   checkButtonDisable:boolean = true;
   showLoading = false;
+  resultsObtained:boolean=false;
   @ViewChild('resultsContainer', { read: ViewContainerRef }) resultsContainer!: ViewContainerRef;
   resultsComponent!: ResultsComponent;
-
   constructor(private route:ActivatedRoute, private router:Router, private location:Location) {}
 
   ngOnInit() {
@@ -30,33 +30,38 @@ export class AppComponent implements OnInit {
     })
   }
 
+  resetApp() {
+    this.nameToCheck = "";
+    this.resultsComponent?.reset();
+    this.checkButtonDisable = true;
+    this.resultsObtained = false;
+    this.updateUrl(this.nameToCheck);
+  }
+
   nameToCheckChanged() {
     this.checkButtonDisable = this.nameToCheck == '';
   }
-
 
   async checkClick() {
     if (this.resultsComponent!=null) this.resultsComponent 
     this.searchDisable = true;
     this.showLoading = true;
-    this.updateUrl();
+    this.updateUrl(this.nameToCheck);
     if (this.resultsContainer.length===0) await this.loadResults(); 
     this.resultsComponent.checkName(this.nameToCheck);
+    this.resultsObtained = this.resultsComponent.results.length>=0;
     this.showLoading=false;
     this.searchDisable= false;
     this.checkButtonDisable = true;
   }
 
-  updateUrl() {
-    const queryParams = {
-      name: this.nameToCheck
-    };
+  updateUrl(nameQuery:string) {
     const urlTree = this.router.createUrlTree([],{
       relativeTo: this.route,
-      queryParams: queryParams,
+      queryParams: nameQuery? { name: nameQuery } : null,
       queryParamsHandling: 'merge'
     });
-    this.location.go(urlTree.toString());
+    this.location.go(nameQuery? urlTree.toString() : "/");
   }
 
   async loadResults() {
